ee9cdf48e3affb2dde490f35c13e701883653a73
Tom Leach <tom@gc.io>
2014-06-16 18:45:46 -0400

    Implemented a basic key-path-based forward/backward dictionary translator.

--- tests/translator_tests.py
+++ tests/translator_tests.py
@@ -1,8 +1,8 @@
 from unittest import TestCase
-from transl8.translator import Translator
+from transl8.translator import Translator, get_value_at_path, set_value_at_path
 
 
-class TranslatorTest(TestCase):
+class TranslatorValidationTest(TestCase):
     def test_valid_spec(self):
         Translator({
             "some_field": "some_other_field"
@@ -13,3 +13,122 @@ class TranslatorTest(TestCase):
             Translator({
                 "some_field": {"a": "dict"}
             })
+
+
+class TestGetValueAtPath(TestCase):
+    def test_value_at_root(self):
+        found, value = get_value_at_path({'a': 1}, 'a')
+        self.assertTrue(found)
+        self.assertEqual(value, 1)
+
+    def test_value_at_root_missing(self):
+        found, value = get_value_at_path({'a': 1}, 'b')
+        self.assertFalse(found)
+        self.assertEqual(value, None)
+
+    def test_value_at_leaf(self):
+        found, value = get_value_at_path({'a': {'b': {'c': 2}}}, 'a.b.c')
+        self.assertTrue(found)
+        self.assertEqual(value, 2)
+
+    def test_value_at_leaf_missing(self):
+        found, value = get_value_at_path({'a': {'b': {'d': 2}}}, 'a.b.c')
+        self.assertFalse(found)
+        self.assertEqual(value, None)
+
+    def test_branch_missing(self):
+        found, value = get_value_at_path({'a': {'d': {'c': 2}}}, 'a.b.c')
+        self.assertFalse(found)
+        self.assertEqual(value, None)
+
+    def test_expected_branch_is_not_a_branch(self):
+        found, value = get_value_at_path({'a': {'b': 'a string'}}, 'a.b.c')
+        self.assertFalse(found)
+        self.assertEqual(value, None)
+
+
+class TestSetValueAtPath(TestCase):
+    def test_value_at_root(self):
+        end = {}
+        set_value_at_path(end, 'a', 1)
+        self.assertEqual({'a': 1}, end)
+
+    def test_value_nested(self):
+        end = {}
+        set_value_at_path(end, 'a.b.c', 1)
+        self.assertEqual({'a': {'b': {'c': 1}}}, end)
+
+    def test_value_nest_existing_branches(self):
+        end = {'a': {'e': 3}, 'd': 2}
+        set_value_at_path(end, 'a.b.c', 1)
+        self.assertEqual({'a': {'b': {'c': 1}, 'e': 3}, 'd': 2}, end)
+
+
+
+class TranslatorTest(TestCase):
+    def setUp(self):
+        self.translator = Translator({
+            'a': 'a',
+            'b': 'c',
+            'd': 'e.f',
+            'g.h.i': 'j.k'
+        })
+
+    def test_backward(self):
+        end = {
+            'a': 1,
+            'c': 2,
+            'e': {'f': 3},
+            'j': {'k': 4}
+        }
+        expected = {
+            'a': 1,
+            'b': 2,
+            'd': 3,
+            'g': {'h': {'i': 4}}
+        }
+        self.assertEqual(expected, self.translator.backward(end))
+
+    def test_forward(self):
+        start = {
+            'a': 1,
+            'b': 2,
+            'd': 3,
+            'g': {'h': {'i': 4}}
+        }
+        expected = {
+            'a': 1,
+            'c': 2,
+            'e': {'f': 3},
+            'j': {'k': 4}
+        }
+        self.assertEqual(expected, self.translator.forward(start))
+
+
+    def test_forward_with_missing_start_fields(self):
+        start = {
+            'a': 1,
+            'd': 3,
+            'g': {'h': {'i': 4}}
+        }
+        expected = {
+            'a': 1,
+            'e': {'f': 3},
+            'j': {'k': 4}
+        }
+        self.assertEqual(expected, self.translator.forward(start))
+
+    def test_forward_with_incorrectly_typed_branch_nodes(self):
+        start = {
+            'a': 1,
+            'b': 2,
+            'd': 3,
+            'g': {'h': 'string'}
+        }
+        expected = {
+            'a': 1,
+            'c': 2,
+            'e': {'f': 3}
+        }
+        self.assertEqual(expected, self.translator.forward(start))
+
--- transl8/translator.py
+++ transl8/translator.py
@@ -3,6 +3,33 @@ class TranslatorFormatException(Exception):
     pass
 
 
+def get_value_at_path(obj, path):
+    if isinstance(path, basestring):
+        path = path.split('.')
+
+    if path[0] in obj:
+        if len(path) > 1:
+            if not isinstance(obj[path[0]], dict):
+                return False, None
+            return get_value_at_path(obj[path[0]], path[1:])
+        return True, obj[path[0]]
+
+    return False, None
+
+def set_value_at_path(obj, path, value):
+    if isinstance(path, basestring):
+        path = path.split('.')
+    if len(path) > 1:
+        set_value_at_path(obj.setdefault(path[0], {}), path[1:], value)
+    else:
+        obj[path[0]] = value
+
+def translate(from_path, to_path, from_source, to_dest):
+    found, value = get_value_at_path(from_source, from_path)
+    if found:
+        set_value_at_path(to_dest, to_path, value)
+
+
 class Translator(object):
     def __init__(self, spec):
         self.validate(spec)
@@ -13,6 +40,22 @@ class Translator(object):
         Checks that the given spec is a valid spec that can be used by
         the Translator class.
         """
-        for from_key, to_key in spec.iteritems():
-            if not isinstance(to_key, basestring):
+        for from_path, to_path in spec.iteritems():
+            if not isinstance(to_path, basestring):
                 raise TranslatorFormatException()
+
+        # TODO:
+        #  - check that we don't map a given path both as a lead and branch
+
+
+    def forward(self, start):
+        end = {}
+        for from_path, to_path in self.spec.iteritems():
+            translate(from_path, to_path, start, end)
+        return end
+
+    def backward(self, end):
+        start = {}
+        for from_path, to_path in self.spec.iteritems():
+            translate(to_path, from_path, end, start)
+        return start
